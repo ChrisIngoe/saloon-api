6eb20a2b2386af54d051c4ae6d2f8e51eeb0bf8d
Chris Ingoe <chris.software.engineer@gmail.com>
2020-10-25 16:34:59 +0000

    Add linting git prehook

--- app/routes/healthcheck.js
+++ app/routes/healthcheck.js
@@ -1,8 +1,8 @@
 'use strict';
 
 exports.index = function (req, res) {
-  /*console.log(
+  console.log(
     'test a really really really really really really really really really really really really really really really really really really really really really really really long literal string',
-  );*/
+  );
   return res.status(200).json({ service: 'OK' });
 };
