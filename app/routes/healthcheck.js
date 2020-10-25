43a4ed7ddddf7582e875ebbe5292da36d34de5bd
Chris Ingoe <chris.software.engineer@gmail.com>
2020-10-25 19:53:55 +0000

    test pre-commit script

--- app/routes/healthcheck.js
+++ app/routes/healthcheck.js
@@ -1,6 +1,8 @@
 'use strict';
 
 exports.index = function (req, res) {
-const val = 'this is a really long string'.includes('test')    ? 'this is another long text'    : 'this is  long piece of text';
-    return res.status(200).json({ service: 'OK', val: val });
+  const val = 'this is a really long string'.includes('test')
+    ? 'this is another long text'
+    : 'this is  long piece of text';
+  return res.status(200).json({ service: 'OK', val: val });
 };
