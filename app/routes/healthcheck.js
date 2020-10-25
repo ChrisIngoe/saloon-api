de8b21767e7eff3d84c6783a24187edb133e03f6
Chris Ingoe <chris.software.engineer@gmail.com>
2020-10-25 19:16:31 +0000

    test pre-commit script

--- app/routes/healthcheck.js
+++ app/routes/healthcheck.js
@@ -2,7 +2,7 @@
 
 exports.index = function (req, res) {
   console.log(
-    'test a   really really really really really really really really really really really really really really really really really really really really really really really long literal string',
+    'test a really really really really really really really really really really really really really really really really really really really really really really really long literal string',
   );
   return res.status(200).json({ service: 'OK' });
 };
