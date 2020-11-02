9f4b397cf3caed3eca674ad9e03274a757726196
Chris Ingoe <chrs.software.engineer@gmail.com>
2020-11-02 11:31:11 +0000

    test commit

--- app/routes/barman.js
+++ app/routes/barman.js
@@ -2,6 +2,8 @@
 
 exports.single = function (req, res) {
   return res.status(200).json({ name: 'Moonshine' });
+  var test = 'asdjkas lajksdasdjk lakjsd alksdj lka jsdklajsd lkj asdlkaj sdklajs dl aklsdj aklsdja lsdkja sdas  aasd asd asd asd ad asd asd as ';
+  console.log(test);
 };
 
 exports.double = function (req, res) {
