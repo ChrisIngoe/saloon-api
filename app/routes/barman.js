c7c5825a99156c6aabf9261db926ea6059a04bcf
Chris Ingoe <chrs.software.engineer@gmail.com>
2020-11-02 11:43:23 +0000

    test commit

--- app/routes/barman.js
+++ app/routes/barman.js
@@ -1,9 +1,8 @@
 'use strict';
 
 exports.single = function (req, res) {
-  var test =
-    'asdjkas lajksdasdjk dfdf dlakjsd alksdj lk dfd a jsdklajsd lkj asdlkaj sdklajs dl aklsdj aklsdja lsdkja sdas  aasd asd asd asd ad asd asd as ';
-  return res.status(200).json({ name: 'Moonshine', test });
+  var test = {name: 'asdjkas lajksdasdjk dfdf dlakjsd alksdj lk dfd a jsdklajsd lkj asdlkaj sdklajs dl aklsdj aklsdja lsdkja sdas  aasd asd asd asd ad asd asd as '};
+  return res.status(200).json({ name: 'Moonshine', test: test });
 };
 
 exports.double = function (req, res) {
